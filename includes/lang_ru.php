860562cc82dcb2639c211940f72c229ee0311636
Christopher Coss <kosscielsky@yandex.com>
2021-02-22 14:41:03 -0600

    Create lang_ru.php

---
+++ includes/lang_ru.php
@@ -0,0 +1,97 @@
+<?php
+/*
+|
+| Заголовки
+|
+*/
+$langMainTitle                          = "НОВОЕ ДОСТИЖЕНИЕ";
+$langSuccessMainTitle                   = "ДОСТИЖЕНИЕ СОХРАНЕНО";
+$langFormLabelCategory                  = "ТИП ДОСТИЖЕНИЯ";
+$langFormLabelDisplayname               = "НАЗВАНИЕ";
+$langFormLabelMessage                   = "ОПИСАНИЕ";
+$langFormLabelTier                      = "УРОВЕНЬ ДОСТИЖЕНИЯ";
+$langFormLabelMeta                      = "МАТЕРИАЛ ИЛИ ОБЪЕКТ";
+$langFormLabelObjective                 = "ЗАДАЧА";
+$langFormLabelRewards                   = "НАГРАДА";
+
+/*
+|
+| Категории
+|
+*/
+$langGroupComplex                       = "Комплексные достижения";
+$langBlockPlace                         = "Размещено блоков";
+$langCatBlockBreak                      = "Разрушено блоков";
+$langCatEntityKills                     = "Совершено убийств";
+$langCatTargetShot                      = "Выстрелов по цели";
+$langCatPlayerCrafts                    = "Совершено крафтов";
+$langCatAnimalBreeding                  = "Выведено животных";
+$langGroupSimple                        = "Простые достижения";
+$langCatPlayerConnections               = "Подключений";
+$langCatPlayerDeaths                    = "Смертей игрока";
+$langCatUsedArrows                      = "Использовано стрел";
+$langCatThrownSnowballs                 = "Брошено снежков";
+$langCatThrownEggs                      = "Брошено яиц";
+$langCatCapturedFish                    = "Поймано рыбы";
+$langCatCapturedTreasures               = "Обнаружено сокровищ";
+$langCatItemBreaks                      = "Сломано предметов";
+$langCatEatenItems                      = "Съедено";
+$langCatAnimalShear                     = "Сострижено";
+$langCatCowMilking                      = "Подоено коров";
+$langCatBucketWater                     = "Ведер с водой";
+$langCatBucketLava                      = "Ведер с лавой";
+$langCatItemEnchantments                = "Зачаровано предметов";
+$langCatVillagerTrades                  = "Сделок с жителями";
+$langCatUsedAnvils                      = "Взаимодействий с наковальней";
+$langCatUsedBeds                        = "Выспался в кровати";
+$langCatMaxLevel                        = "Уровней опыта";
+$langCatUsedPotions                     = "Использовано зелий";
+$langCatPlayedTime                      = "Проведено в игре";
+$langCatDistanceFoot                    = "Преодолено пешком";
+$langCatDistancePig                     = "Преодолено верхом на свинье";
+$langCatDistanceHorse                   = "Преодолено верхом на лошади";
+$langCatDistanceBoat                    = "Преодолено на лодке";
+$langCatDistanceMinecart                = "Преодолено в вагонетке";
+$langCatDistanceGliding                 = "Преодолено на элитрах";
+$langCatDistanceLlama                   = "Преодолено верхом на ламе";
+$langCatDistanceSneaking                = "Преодолено крадучись";
+$langCatItemDrops                       = "Выброшено предметов";
+$langCatItemPickup                      = "Подобрано предметов";
+$langCatWorldPlow                       = "Вскопано";
+$langCatWorldFertilising                = "Удобрено";
+$langCatAnimalTaming                    = "Приручено животных";
+$langCatUsedFireworks                   = "Использовано фейерверков";
+$langCatUsedMusic                       = "Проиграно пластинок";
+$langCatUsedPearls                      = "Использовано Жемчугов Края";
+$langCatUsedFurnance                    = "Выплавлено предметов";
+$langTierCommon                         = "Обычное";
+$langTierRare                           = "Редкое";
+
+/*
+|
+| Награды
+|
+*/
+$langRewardMoney                        = "Деньги";
+$langRewardExperience                   = "Опыт";
+$langRewardOxygen                       = "Кислород (Дыхание)";
+$langRewardHealth                       = "Здоровье";
+
+/*
+|
+| Кнопки
+|
+*/
+$langBtnSubmit                          = "Сгенерировать";
+$langBtnClear                           = "Сбросить";
+$langBtnNewAchievement                  = "Создать новое достижение";
+
+/*
+|
+| Сообщения и тексты
+|
+*/
+$langSuccessMessage                     = "ВАШЕ НОВОЕ ДОСТИЖЕНИЕ СОЗДАНО";
+$langSuccessDescription                 = "После создания достижений вы найдете сгенерированные достижения, отсортированные по типу в папке builder/content.";
+$langFormDescOxygen                     = "Помните: если вы выберете кислород в качестве награды, каждый кислородный пузырь будет равен 30.";
+?>
